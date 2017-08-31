082a308a8a36b6045ea0484fd79656459af20b58
s-r-aman <s.r.aman.sra@gmail.com>
2017-08-31 17:14:23 +0530

    Adding some more programs.
    These are the programs for the circular and doubly Linked list, Do let me know the errors and bugs.

---
+++ Insertion of the element by automatic sorting (Double Linked List).c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+struct node *start;
+
+/*------Function to Print a linked list------*/
+void print(){
+    struct node *temp;
+     temp = start;
+     while(temp!=NULL){
+        printf(" %d", temp->data);
+        temp = temp->next;
+     }
+
+}
+
+/*------Function to insert data into a linked list------*/
+void insert(int x){
+    struct node *tempins = (struct node *)malloc(sizeof(struct node));
+    struct node *temp1;
+
+    if(start==NULL){                                                    //If the list is empty
+        start = tempins;
+        tempins->data = x;
+        tempins->next = NULL;
+        tempins->prev = NULL;
+    }else{
+            temp1 = start;
+
+            if(temp1->next == NULL){                                    //List has at least one element
+                if((temp1->data) >= x){                                 //If it has only one element
+                    tempins->next = start;
+                    temp1 -> prev = tempins;
+                    tempins -> prev = NULL;
+                    start = tempins;
+                    tempins->data =x;
+
+                    }else{
+                        temp1 ->next = tempins;
+                        tempins->next = NULL;
+                        tempins -> prev = temp1;
+                        tempins->data = x;
+                    }
+                }else{                                                  //List has 2 or more that 2 elements
+
+                    if((temp1->data) >= x){                             //Checking it if it is smaller then the first element
+                        tempins->next = start;
+                        start -> prev = tempins;
+                        tempins -> prev = NULL;
+                        start = tempins;
+                        tempins->data = x;
+                    }else{                                               //Checking in the other elements
+                        while((temp1->next->data)  <= x){                //Loop is for traversing the list
+                            if(temp1->next->next == NULL){
+                            temp1=temp1->next;
+                            temp1->next = tempins;
+                            tempins->next=NULL;
+                            tempins->data =x;
+                            return;
+                            }else{
+                                temp1=temp1->next;
+
+                            }
+
+                        }
+
+                        tempins->next = temp1->next;
+                        tempins->prev = temp1;
+                        temp1->next->prev = tempins;
+                        temp1->next = tempins;
+                        tempins->data = x;
+                    }
+
+                }
+
+
+
+        }
+
+    }
+
+
+
+int main(){
+    start = NULL;
+    insert(1); //Calling the function insert
+    insert(2);
+    insert(3);
+    insert(4);
+    insert(7);
+    insert(6);
+    insert(1);
+    insert(3);
+    print();   //Calling Function to print
+}
